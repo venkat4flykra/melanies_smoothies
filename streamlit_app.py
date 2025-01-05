2970bd11bc88002caff71780c3a8ed3131dfb430
venkat4flykra <Venkat@flykra.io>
2025-01-04 21:56:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 name_on_order = st.text_input('Name on smoothie : ')
 st.write('Your name on your smoothie order will be : ' , name_on_order)
 #session = get_active_session()
-cnx = st.connection("snwflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
